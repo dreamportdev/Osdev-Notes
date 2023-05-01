e4c53e3736306094d238f64d122265ba3cdd1380
Ivan Gualandri <dreamos82@yahoo.it>
2023-05-01 18:38:13 +0100

    Try to automate files list generation

--- build.sh
+++ build.sh
@@ -2,9 +2,23 @@
 
 result=`find . -maxdepth 1 -type d | sort`
 
+decorators=($(ls -d .pandoc/decorators/*))
+counter=0
+fileslit=""
 echo $result
+echo ${decorators[@]}
+echo ${decorators[0]}
+echo ${decorators[10]}
 
 while IFS='' read -r -d '' filename; do
   : # something with "$filename"
-  echo $filename
+  if [ $filename = "." ]; then
+    continue
+  fi
+  echo $filename "- ${counter}"
+  fileslist=$fileslist" "`ls -d $filename/*`" ${decorators[$counter]}"
+  ((counter+=1))
 done < <(find .  -maxdepth 1 -not -path '*/.*' -type d -print0 | sort -zV )
+
+echo "=-===="
+echo $fileslist
