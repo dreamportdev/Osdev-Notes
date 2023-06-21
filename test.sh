62da9b22f1d21115977051a9527f6d6740b2ed93
Dean <dean243@hotmail.com>
2023-06-21 20:16:09 +1000

    renamed decorators, added alternate test script

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+get_entries() {
+    echo $(find $1 -maxdepth 1 -regextype egrep -regex '.*\/[0-9]{2}_[A-Za-z_.]*' | sort)
+}
+
+pandoc_filename="osdev-notes-$(date +%F).pdf"
+pandoc_flags="--resource-path=$(pwd) --toc --top-level-division=chapter -f markdown+raw_tex -t pdf --pdf-engine=pdflatex -N --lua-filter .pandoc/makerelative.lua"
+
+echo "Building pandoc command line. Structure of the final pdf is based on the directory structure."
+echo "Each directory in the format \"XX_Name\" (where XX is two digits) is included."
+echo "|-- Title"
+echo "|-- Table of Contents"
+echo "|"
+
+chapter_dirs="$(get_entries '.')"
+for dir in $chapter_dirs; do
+    if [ -f ".pandoc/decorators/${dir:2}.tex" ]; then
+        cmd_body+=".pandoc/decorators/${dir:2}.tex "
+        echo "|---- ${dir:2} (with decorator)"
+    else
+        echo "|---- ${dir:2} (no decorator file)"
+    fi
+
+    chapter_entries="$(get_entries $dir)"
+    for entry in $chapter_entries; do
+        if [[ $entry != *".md" ]]; then
+            continue;
+        fi
+        echo "|  |- ${entry:2}"
+        cmd_body+="$entry "
+    done
+    unset chapter_entries
+    echo "|"
+done
+
+cmd_body+="LICENSE.md"
+$(pandoc $pandoc_flags $cmd_body .pandoc/pandoc.yaml -o $pandoc_filename)
+
