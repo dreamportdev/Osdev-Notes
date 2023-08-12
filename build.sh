ff184893e325c4233ba11b2a4662b3c41ffdc48c
Ivan Gualandri <dreamos82@yahoo.it>
2023-08-12 02:05:08 +0100

    Rename test.sh in build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
-result=`find . -maxdepth 1 -type d | sort`
+get_entries() {
+    echo $(find $1 -maxdepth 1 -regextype egrep -regex '.*\/[0-9A-Z]{1,2}_[A-Za-z_.]*' | sort)
+}
 
-decorators=($(ls -d .pandoc/decorators/*))
-counter=0
-fileslit=""
-output_file="osdev_notes_book.pdf"
-pandoc_flags="--toc --top-level-division=chapter -f markdown+raw_tex -t pdf --pdf-engine=pdflatex -N --lua-filter .pandoc/makerelative.lua"
-pandoc_command="pandoc ${pandoc_flags}"
-exclusion_pattern="/README.md"
+pandoc_filename="osdev-notes-$(date +%F).pdf"
+pandoc_flags="--resource-path=$(pwd) --toc --top-level-division=chapter -f markdown+raw_tex -t pdf --pdf-engine=pdflatex -N --lua-filter .pandoc/makerelative.lua"
 
-echo $result
-echo ${decorators[@]}
-echo ${decorators[0]}
-echo ${decorators[10]}
+echo "Building pandoc command line. Structure of the final pdf is based on the directory structure."
+echo "Each directory in the format \"XX_Name\" (where XX is two digits) is included."
+echo "|-- Title"
+echo "|-- Table of Contents"
+echo "|"
 
-while IFS='' read -r -d '' filename; do
-  : # something with "$filename"
-  if [ $filename = "." ] || [ $filename = "./Images" ]; then
-    continue
-  fi
+chapter_dirs="$(get_entries '.')"
+for dir in $chapter_dirs; do
+    if [ -f ".pandoc/decorators/${dir:2}.tex" ]; then
+        cmd_body+=".pandoc/decorators/${dir:2}.tex "
+        echo "|---- ${dir:2} (with decorator)"
+    else
+        echo "|---- ${dir:2} (no decorator file)"
+    fi
 
-  echo $filename "- ${counter}"
-  fileslist=$fileslist" "`ls -R -d $filename/*`" ${decorators[$counter]}"
-  #fileslist=$fileslist" "`find ${filename} -type f -name '*.md' | sort`" ${decorators[$counter]}"
-  ((counter+=1))
-done < <(find .  -maxdepth 1 -not -path '*/.*' -type d -print0 | sort -zV )
+    chapter_entries="$(get_entries $dir)"
+    for entry in $chapter_entries; do
+        if [[ $entry != *".md" ]]; then
+            continue;
+        fi
+        echo "|  |- ${entry:2}"
+        cmd_body+="$entry "
+    done
+    unset chapter_entries
+    echo "|"
+done
 
-echo "=-===="
-pandoc_command="$pandoc_command $fileslist LICENSE.md .pandoc/pandoc.yaml -o $output_file"
+cmd_body+="LICENSE.md"
+$(pandoc $pandoc_flags $cmd_body .pandoc/pandoc.yaml -o $pandoc_filename)
 
-echo $pandoc_command
-eval $pandoc_command
